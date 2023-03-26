90c2995f94098b01073968d43ef63aa6066e7726
Cornelius Venti <corneliusventi@gmail.com>
2023-03-26 11:57:18 +0700

    fix filterUserForClient import

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -2,7 +2,7 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { filterUserForClient } from "~/server/helpers/FilterUserForClient";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 export const profileRouter = createTRPCRouter({
   getUserByUsername: publicProcedure
